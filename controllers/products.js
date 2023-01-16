005dfc5ebcdc3c77fa8ff0855ce3c328c4cfddbb
coderrohan14 <Rahkins@1996>
2023-01-17 01:29:22 +0530

    Changes in routes

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,6 @@
 const Product = require("../models/product")
 
-
-const getAllProductsStatic = async (req, res) => {
+const getAllProducts = async (req, res) => {
     const { featured, company, name, sort, fields, page, limit, numericFilters } = req.query
     const queryObject = {}
 
@@ -61,12 +60,6 @@ const getAllProductsStatic = async (req, res) => {
     res.status(200).json({ products, nbHits: products.length })
 }
 
-const getAllProducts = async (req, res) => {
-    const products = await Product.find({ name: { $regex: req.query.name } })
-    res.status(200).json({ products, nbHits: products.length })
-}
-
 module.exports = {
-    getAllProducts,
-    getAllProductsStatic
+    getAllProducts
 }
